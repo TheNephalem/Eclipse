2a80ef9e6ac1e47fcc704edff319d1429cab929b
Kristiyan Kralev <kristiyankralev@gmail.com>
2016-04-29 19:58:47 +0200

    Add a simple output change.

---
+++ TestProject/src/test/Main.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Main {
+	public static void main(String[] args) {
+		SalesTool tool = new SalesTool();
+		
+		display();
+		tool.displayData();
+	}
+	
+	public static void display() {
+		System.out.println("United we stand");
+		System.out.println("Divided we fall");
+	
+		System.out.println("Test 1");
+	}
+}
